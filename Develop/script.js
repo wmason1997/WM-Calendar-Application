9787ff9dfac11d80eeaa3013e590aa7e1cb9a246
wmason1997 <williamcmason1997@gmail.com>
2023-11-03 16:59:16 -0700

    Added the displayDate function. Need to add the advanced Formatting and move it inside our bigger function later on.

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,11 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var dayDisplayEl = $('#currentDay');
+
+// gotta wrap in jQuery call
+
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -20,4 +25,18 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  function displayDate() {
+    var dayToday = dayjs().format('dddd, MMM D'); // have to import advancedFormat to add the o after the D
+    dayDisplayEl.text(dayToday);
+
+  }
 });
+
+
+function displayDate() {
+  var dayToday = dayjs().format('dddd, MMM D'); // have to import advancedFormat to add the o after the D and have to move inside
+  dayDisplayEl.text(dayToday);
+
+}
+
+displayDate();
